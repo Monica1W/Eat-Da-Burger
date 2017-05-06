5f59f2434755ca76548cf8cf1fcd8c20263d14d7
Monica Woodie <monicawoodie@gmail.com>
2017-05-05 20:14:48 -0400

    updating format

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ use fs3lyc8zxsxbe5hp;
 create table burgers (
 	id INT AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR(100) NOT NULL,
-    devoured boolean DEFAULT true NOT NULL,
+    devoured boolean DEFAULT true,
     createdAt TIMESTAMP NOT NULL,
     PRIMARY KEY(id)
  );
